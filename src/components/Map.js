9e5ae7b2c8c5211d029566efaee2ab953a96dab7
Brand0nKelly <71873543+Brand0nKelly@users.noreply.github.com>
2022-12-13 13:37:45 +0000

    Add files via upload

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,79 +1,164 @@
 import React, { useState,useEffect} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import Navbar from '../components/Navbar';
+import L from "leaflet";
+import "leaflet.markercluster";
+import $ from "jquery";
 import {useCookies} from 'react-cookie';
-import '../index.css';
-import '../Images/marker-icon-2x.png';
-import createGolfCourseMarkers from "./overpass";
 
-import { Icon } from 'leaflet'
+  const Map = () => {
+  const [def_pos] = useState({ lat: 53.4417455, lng: -6.1527245 });
+  const [map, setMap] = useState(null);
+  const [fuel, setFuel] = useState("");
+
+  var iconMarker;
+  var fuel_markers;
 
 
+  const icon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+    iconSize: [25, 35],
+    iconAnchor: [12, 40],
+    popupAnchor: [1, -33],
+  });
 
-const MyMap = () => {
-    const [latLng, setLatLng] = useState(null);
-    const [map, setMap] = useState(null);
-    const [geojson, setGeojson] = useState(null);
+
+  function Maplocation(map) 
+  {
+    updateLocation(map);
+  }
+
+  function updateLocation(map) {
+    navigator.geolocation.getCurrentPosition(position => {
+      setMapToCurrentLocation(map, position);
+      database_updated(position);
+    });
+  }
   
-    useEffect(() => {
-      // Get the user's current location using the Geolocation API
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setLatLng([latitude, longitude]);
+  //  useEffect(() => {
+  //    navigator.geolocation.getCurrentPosition((position) => {
+  //     setMapToCurrentLocation(map, position);
+  //     databaseUpdated(position);
+  //   });
+  // }, []);
 
 
-        fetch('http://127.0.0.1:8001/api/updated-location/', {
+
+  // useEffect(() => {
+  //    if (latLng) {
+ // const mapInstance = L.map('map').setView(latLng, 16);
+  //      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+//    }).addTo(mapInstance);
+  
+  function setMapToCurrentLocation(map, position) 
+  {
+    var latLng = L.latLng(position.coords.latitude, position.coords.longitude);
+    
+
+    if (iconMarker) {
+      map.removeLayer(iconMarker);
+    }
+
+    iconMarker = L.marker(latLng, { icon}).addTo(map);
+
+
+    iconMarker.bindPopup(
+        `Current Location <br> Latitude: ${latLng.lat} <br> Longitude: ${latLng.lng}`
+      ).openPopup();
+}  
+
+  async function database_updated(pos) 
+  {
+    const coords = `${pos.coords.longitude}, ${pos.coords.latitude}`;
+    
+    try {
+       await fetch('http://127.0.0.1:8001/api/updated-location/', {
             method: 'POST',
             mode: 'cors',
-            body: JSON.stringify({ latitude, longitude, }),
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: 'token 1885d1bffdd41e7aa20ed9bad273973191b4b63b',
-            },
-  
-            
-          });
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ coords }),
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'token 1885d1bffdd41e7aa20ed9bad273973191b4b63b',
+        },
 
-
+    }).then((response) => {
+        if (response.ok) {
+          console.log("Sucess");
+        }
       });
-    }, []);
+    } catch (err) { console.error(err);
+    }
+  }
+
+  async function showPoiMarkers() 
+  {
+
+
+  
+    if (fuel_markers) {
+      map.removeLayer(fuel_markers);
+    }
 
 
 
-    useEffect(() => {
-      if (latLng) {
-        const mapInstance = L.map('map').setView(latLng, 16);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        }).addTo(mapInstance);
-
-
-
-
-
-
-const icon = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
-
-<Popup>
-    Your location is: {latLng}
-</Popup>
-
-const marker = L.marker(latLng, { icon }).addTo(mapInstance);
-        setMap(mapInstance);
+    $.ajax({
+        type: "POST",
+        headers: {"Authorization": "token 1885d1bffdd41e7aa20ed9bad273973191b4b63b"},
+        url: "http://127.0.0.1:8001/api/golf/",
+       data: {
+        query: fuel,
+        bbox: map.getBounds().toBBoxString(),
       }
-    }, [latLng]);
-  
+      ,
+    })
+      .done(function (data, xhr, status) {
 
-    
-    return <div id="map" style={{ height: '750px', width: '100%' , marginTop:'2%' }} />;
-  };
-  
+        fuel_markers = L.markerClusterGroup();
 
-export default MyMap;
+        
+        var geoJsonLayer = L.geoJson(data, {
+          pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, { icon });
+          },
+          onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+          },
+        });
+        fuel_markers.addLayer(geoJsonLayer);
+
+
+        map.addLayer(geoJsonLayer);
+      })
+      // fail(function(xhr, status, error){
+      //   var message = "Overpass query has failed<br/>";
+     //    console.log("Status" + xhr.status + " " + xhr.responseText);
+     //    showOkAlert(message);
+   //  });
+   
+  }
+
+  function handlePoiChange(event) {
+    setFuel(event.target.value);
+  }
+
+  Maplocation(map);
+return (
+    <div className="Mapclassdiv">
+      <div id="styleMap">
+        <MapContainer center={def_pos} zoom={16} ref={setMap}>
+          <TileLayer
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a </a>'
+          />
+        </MapContainer>  
+        <input type="text" size="20" onChange={handlePoiChange}/>
+        <button type="submit" onClick={showPoiMarkers}>Find Fuel</button>  
+
+      </div>
+</div>
+
+  );
+};
+
+export default Map;
